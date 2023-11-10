8d59172b0fe79bf0e96d4ebe16adf4cb26e6d842
unknown <abhinavnehra2203@gmail.com>
2023-11-10 12:21:19 -0600

    Discovery method changed from mDNS to Broadcast

--- Client(Sender).py
+++ Client(Sender).py
@@ -1,39 +1,51 @@
 import socket
-from zeroconf import Zeroconf, ServiceBrowser
+import time
 
-class ServiceListener:
-    def remove_service(self, zeroconf, type, name):
-        pass
+def send_file(ip, port, file_path):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((ip, port))
 
-    def add_service(self, zeroconf, type, name):
-        info = zeroconf.get_service_info(type, name)
-        if info:
-            self.connect_to_server(socket.inet_ntoa(info.address), info.port)
+    with open(file_path, 'rb') as file:
+        while True:
+            data = file.read(1024)
+            if not data:
+                break
+            client_socket.sendall(data)
 
-    def connect_to_server(self, server_ip, server_port):
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect((server_ip, server_port))
+    print(f"File sent successfully: {file_path}")
 
-        file_to_send = input("Enter the path of the file to send: ")
+    client_socket.close()
 
-        with open(file_to_send, 'rb') as file:
-            while True:
-                data = file.read(1024)
-                if not data:
-                    break
-                client_socket.sendall(data)
+def discover_server(port, timeout=5):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-        print(f"File sent successfully: {file_to_send}")
+    client_socket.bind(('0.0.0.0', 0))
 
-        client_socket.close()
+    message = b"DISCOVER_SERVER"
+
+    start_time = time.time()
+
+    while time.time() - start_time < timeout:
+        client_socket.sendto(message, ('<broadcast>', port))
+        try:
+            data, server_address = client_socket.recvfrom(1024)
+            if data.decode() == "SERVER_FOUND":
+                return server_address[0]
+        except socket.error:
+            pass
+
+    return None
 
 if __name__ == "__main__":
-    zeroconf = Zeroconf()
-    listener = ServiceListener()
-    browser = ServiceBrowser(zeroconf, "_filetransfer._tcp.local.", listener)
+    server_port = int(input("Enter the server port: "))
+    file_to_send = input("Enter the path of the file to send: ")
 
-    try:
-        while True:
-            pass
-    except KeyboardInterrupt:
-        zeroconf.close()
+    # Discover the server IP address
+    server_ip = discover_server(server_port)
+
+    if server_ip:
+        print(f"Server found at {server_ip}")
+        send_file(server_ip, server_port, file_to_send)
+    else:
+        print("Server not found.")
--- Server(Receiver).py
+++ Server(Receiver).py
@@ -1,18 +1,11 @@
 import socket
-from zeroconf import ServiceInfo, Zeroconf
 
 def receive_file(port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(('0.0.0.0', port))
     server_socket.listen(1)
 
-    zeroconf = Zeroconf()
-    info = ServiceInfo("_filetransfer._tcp.local.",
-                       f"FileTransferServer._filetransfer._tcp.local.",
-                       socket.inet_aton(socket.gethostbyname(socket.gethostname())), port, 0, 0, {})
-    zeroconf.register_service(info)
-
-    print(f"Server waiting for discovery on port {port}...")
+    print(f"Server listening on port {port}...")
 
     client_socket, client_address = server_socket.accept()
     print(f"Connection from {client_address}")
@@ -28,11 +21,9 @@ def receive_file(port):
 
     print(f"File received successfully: {file_name}")
 
-    zeroconf.unregister_service(info)
-    zeroconf.close()
     client_socket.close()
     server_socket.close()
 
 if __name__ == "__main__":
-    port = 65432
+    port = int(input("Enter the port to listen on: "))
     receive_file(port)
